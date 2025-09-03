ad9c130a177c9573f871397daa14fb73beb54cf9
Nike <nikehallberg9@gmail.com>
2025-09-03 11:28:30 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Movies from "./components/movies/Movies";
 import Shows from "./components/shows/Shows";
 import Songs from "./components/songs/Songs";
 import { Route, Routes } from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'react-bootstrap';
 
 const App = () => {
   return (
